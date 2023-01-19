037c173459ffc2ea17a3bff8453b95fb8752748c
PierreGoupil13 <p.goupil356@gmail.com>
2023-01-19 19:19:13 +0100

    Creation test palindrome et retour "Bien dit". Les test sont rouge car la fonction n'est pas encore implémenté.

--- Etape_1/oche_test.py
+++ Etape_1/oche_test.py
@@ -7,12 +7,25 @@ class OhceTest(unittest.TestCase):
     # Test miroir
     @parameterized.parameterized.expand(["toto", "test"])
     def test_miroir(self,mot):
-        # Quand on saisit une chaine
+        # QUAND on saisit une chaine
         ohce = Ohce()
         retour = ohce.miroir(mot)
 
-        # Alors celle-ci est renvoyée en miroir
+        # ALORS celle-ci est renvoyée en miroir
         self.assertIn(mot[::-1], retour)
+        
+    @parameterized.parameterized.expand(["radar", "kayak"])
+    def test_palindrome(self,mot):
+        # QUAND on saisit un palindrome
+        ohce = Ohce()
+        retour = ohce.miroir(mot)
+
+        # ALORS celui-ci est renvoyé
+        retour_palindrome = ohce.miroir(mot)
+        self.assertIn(mot, retour_palindrome)
+
+        # ET 'Bien dit' est renvoyé ensuite
+        self.assertIn("Bien dit",retour_palindrome)
 
 if __name__ == '__main__':
     unittest.main()
--- Etape_1/ohce.py
+++ Etape_1/ohce.py
@@ -1,3 +1,6 @@
 class Ohce:
     def miroir(self,mot):
+        miroir = mot[::-1]
+        if mot == miroir:
+            return ""
         return mot[::-1]
