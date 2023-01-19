ab6adb068ee5e2c40f159c23e4a0afc682a1164b
PierreGoupil13 <p.goupil356@gmail.com>
2023-01-19 19:08:17 +0100

    Premier etapes avec les tests qui se lancent et qui sont rouges.

--- Etape_1/oche_test.py
+++ Etape_1/oche_test.py
@@ -1,14 +1,19 @@
 import unittest
+import parameterized as parameterized
 from ohce import Ohce
+
+
 class OhceTest(unittest.TestCase):
     # Test miroir
+    @parameterized.parameterized.expand(["toto", "test"])
     def test_miroir(self,mot):
-        
         # Quand on saisit une chaine
         ohce = Ohce()
         retour = ohce.miroir(mot)
 
         # Alors celle-ci est renvoyée en miroir
         self.assertIn(mot[::-1], retour)
-        
+
+if __name__ == '__main__':
+    unittest.main()
 
