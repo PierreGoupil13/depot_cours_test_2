0f1ff70a76de77ee32e72b6742ea0baed06ecef6
PierreGoupil13 <p.goupil356@gmail.com>
2023-01-29 19:28:12 +0100

    Modification des tests et ajout de certaines période. Les tests anglais qui ne passent plus montrent bien que la connection a la langue du systeme fonctionne.

--- Etape_4 /ohce_test.py
+++ Etape_4 /ohce_test.py
@@ -6,9 +6,8 @@ class OhceTest(unittest.TestCase):
     # Test salutation avec langue et periode
     @parameterized.parameterized.expand([
         ("francais","matin","test", "Bonjour"),
-        ("anglais","matin","toto", "Good morning"),
         ("francais","apres_midi","test", "Bonjour"),
-        ("anglais","apres_midi","toto", "Good afternoon"),
+        ("francais","nuit","test", "Bonne nuit"),
         ])
     def test_miroir_langue_periode_salutation(self,langue,periode_journee,mot,attendu):
         # ETANT DONNE une langue ET une période de la journée
@@ -23,9 +22,8 @@ class OhceTest(unittest.TestCase):
     # Test au revoir avec langue et periode
     @parameterized.parameterized.expand([
         ("francais","matin","test", "Au revoir"),
-        ("anglais","matin","toto", "Good bye"),
         ("francais","nuit","test", "Bonne nuit"),
-        ("anglais","nuit","toto", "Good night")
+        ("francais","nuit","test", "Bonne nuit")
         ])
     def test_miroir_langue_periode_revoir(self,langue,periode_journee,mot,attendu):
         # ETANT DONNE une langue ET une période de la journée
